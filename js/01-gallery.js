f0ff52a5168486f48d09093eacbc399bc908eec6
Iurii Bardych <iurii_bardych@hotmail.com>
2023-01-28 00:23:53 +0100

    01-gallery done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+for (let galleryitem of galleryItems) {
+  const itemcontainer = document.createElement("div");
+  itemcontainer.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.setAttribute("href", galleryitem.original);
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.setAttribute("src", galleryitem.preview);
+  img.setAttribute("alt", galleryitem.description);
+  img.dataset.source = galleryitem.original;
+
+  link.appendChild(img);
+  itemcontainer.appendChild(link);
+
+  gallery.append(itemcontainer);
+}
+
+gallery.addEventListener("click", showImage);
+
+function showImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  showModal(event.target);
+}
+
+function showModal(imagelink) {
+  const original = imagelink.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+    <div class="modal">
+      <a><img class="modal__image" src="${original}"></a>
+    </div>
+  `,
+    {
+      onShow: (instance) => {
+        instance.element().querySelector("a").onclick = instance.close;
+      },
+    }
+  );
+
+  instance.show();
+}
