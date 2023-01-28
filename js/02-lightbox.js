78c03e49a67a4d11ce4794b42bd8bed40a22b902
Iurii Bardych <iurii_bardych@hotmail.com>
2023-01-28 19:14:21 +0100

    02-lightbox done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+for (let galleryitem of galleryItems) {
+  const itemcontainer = document.createElement("div");
+  itemcontainer.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.setAttribute("href", galleryitem.original);
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.setAttribute("src", galleryitem.preview);
+  img.setAttribute("alt", galleryitem.description);
+
+  link.appendChild(img);
+  itemcontainer.appendChild(link);
+
+  gallery.append(itemcontainer);
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
